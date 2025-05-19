9dc2ebd05b97165aabed54089d66ef07925ca92b
Waqar Khan <waqar.mrwt424@gmail.com>
2025-05-19 16:41:40 +0500

    Initial commit

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,14 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Nunito_Sans } from "next/font/google";
 import Header from "@/components/header";
 import Footer from "@/components/Footer";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const nunitoSans = Nunito_Sans({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  display: "swap",
+  preload: true,
+  variable: "--font-sans",
+  fallback: ["Nunito Sans", "ui-sans-serif"],
 });
 
 export const metadata = {
@@ -20,10 +18,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" className={nunitoSans.className}>
+      <body className="antialiased">
         <Header />
         <main className="min-h-screen">{children}</main>
         <Footer />
